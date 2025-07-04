16dea2a79bca83464f0b0216ac7cd75fb6598960
Steeve Pommier <CostardRouge@users.noreply.github.com>
2025-07-04 11:24:01 +1000

    Add allowedDevOrigins to Next.js config
    Introduces the allowedDevOrigins option with '192.168.1.155' to the Next.js configuration. This may be used to specify allowed development origins for the application.

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import type {
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  allowedDevOrigins: [
+    "192.168.1.155"
+  ],
   crossOrigin: "anonymous",
   eslint: {
     ignoreDuringBuilds: true,
