ad2f10bbad5e390162438cbcf7b94f3b22431452
Steeve Pommier <CostardRouge@users.noreply.github.com>
2025-09-16 23:24:44 +1000

    Add FPS toggle button and update control icons
    Introduces a new button to toggle FPS display using ToggleLeft/ToggleRight icons and updates the play/pause button to control the drawing loop. Also adjusts button order and titles for clarity.

--- src/components/ClientProcessingSketch/components/P5Controls.tsx
+++ src/components/ClientProcessingSketch/components/P5Controls.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  Camera, Pause, ToggleLeft, Play
+  Camera, Play, Pause, ToggleLeft, ToggleRight
 } from "lucide-react";
 
 export function P5Controls( {
@@ -10,20 +10,36 @@ export function P5Controls( {
     setLooping
   ] = React.useState( true );
 
+  const [
+    fps,
+    setFps
+  ] = React.useState( true );
+
   return (
     <div
-      className="text-center absolute p-2 bg-white border-t-0 border border-gray-400 shadow shadow-black-300 drop-shadow-sm rounded-br-sm border-l-0 top-0 left-0 flex gap-1 z-50">
+      className="text-center absolute p-2 bg-white border-t-0 border border-gray-400 shadow shadow-black-300 drop-shadow-sm rounded-br-sm border-l-0 top-0 left-0 flex gap-1 z-50"
+    >
       <>
+        <button
+          title="Show/Hide FPS"
+          onClick={() => {
+            window?.toggleFPS();
+            setFps( fps => !fps );
+          }}
+          className="rounded-sm bg-white border border-gray-400 px-2 py-1 shadow shadow-gray-200 text-sm text-gray-500 hover:text-black"
+        >
+          {fps ? <ToggleLeft className="h-4"/> : <ToggleRight className="h-4"/>}
+        </button>
         <button
           onClick={() => {
             window?.toggleLoop();
             setLooping( looping => !looping );
           }}
-          title="Show/Hide FPS"
+          title="Toogle drawing loop"
 
           className="rounded-sm bg-white border border-gray-400 px-2 py-1 shadow shadow-gray-200 text-sm text-gray-500 hover:text-black"
         >
-          { looping ? <Pause className="h-4"/> : <Play className="h-4"/> }
+          {looping ? <Pause className="h-4"/> : <Play className="h-4"/>}
         </button>
 
         <button
@@ -34,16 +50,6 @@ export function P5Controls( {
           className="rounded-sm bg-white border border-gray-400 px-2 py-1 shadow shadow-gray-200 text-sm text-gray-500 hover:text-black"
         >
           <Camera className="h-4"/>
-        </button>
-
-        <button
-          title="Show/Hide FPS"
-          onClick={() => {
-            window?.toggleFPS();
-          }}
-          className="rounded-sm bg-white border border-gray-400 px-2 py-1 shadow shadow-gray-200 text-sm text-gray-500 hover:text-black"
-        >
-          <ToggleLeft className="h-4"/>
         </button>
       </>
     </div>
