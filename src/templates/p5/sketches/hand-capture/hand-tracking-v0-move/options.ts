8c418046ced180b9cd692158a1894f81b1a7c44a
Steeve Pommier <CostardRouge@users.noreply.github.com>
2026-06-14 13:12:20 -0400

    hand captures previews

--- src/templates/p5/sketches/hand-capture/hand-tracking-v0-move/options.ts
+++ src/templates/p5/sketches/hand-capture/hand-tracking-v0-move/options.ts
@@ -17,7 +17,7 @@ export const formValues = {
       enabled: true,
       hands: {
         ...interactionFormValues.vision.hands,
-        enabled: true,
+        enabled: false,
         landmarks: {
           fingertips: true,
           palm: true
@@ -56,7 +56,7 @@ export const formValues = {
     shadowsCount: 3,
     dotScale: 1,
     // Lower = longer light trails (less is erased each frame).
-    trail: 10
+    trail: 255
   },
 
   // Overlay text
--- src/templates/p5/sketches/hand-capture/hand-tracking-v4-text/options.ts
+++ src/templates/p5/sketches/hand-capture/hand-tracking-v4-text/options.ts
@@ -17,7 +17,7 @@ export const formValues = {
       enabled: true,
       hands: {
         ...interactionFormValues.vision.hands,
-        enabled: true,
+        enabled: false,
         landmarks: {
           fingertips: true,
           palm: true
